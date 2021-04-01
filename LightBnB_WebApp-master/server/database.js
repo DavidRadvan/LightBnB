47c1fda0af16dfec0a509a0896611f662cd2f366
David Radvan <david.radvan@gmail.com>
2021-04-01 10:32:34 -0700

    modify getUserWithEmail, getUserWithId, and addUser to use SQL database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -18,17 +18,15 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
-}
+  return pool.query(`
+   SELECT *
+   FROM users
+   WHERE email = $1
+   `, [email])
+    .then(function(res) {
+      return res.rows[0];
+    }, null);
+};
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -37,7 +35,14 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`
+   SELECT *
+   FROM users
+   WHERE id = $1
+   `, [id])
+    .then(function(res) {
+      return res.rows[0];
+    }, null);
 }
 exports.getUserWithId = getUserWithId;
 
@@ -48,11 +53,16 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-}
+  return pool.query(`
+   INSERT INTO users (name, email, password)
+   VALUES ($1, $2, $3)
+   RETURNING *;
+   `, [user.name, user.email, user.password])
+    .then(function(res) {
+      console.log(res.rows[0]);
+      return res.rows[0];
+    }, null);
+};
 exports.addUser = addUser;
 
 /// Reservations
@@ -77,7 +87,8 @@ exports.getAllReservations = getAllReservations;
  */
 const getAllProperties = function(options, limit = 10) {
   return pool.query(`
-   SELECT * FROM properties
+   SELECT *
+   FROM properties
    LIMIT $1
    `, [limit])
     .then(res => res.rows);
